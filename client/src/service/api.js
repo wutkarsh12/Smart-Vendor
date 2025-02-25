91900fc87178814ddfd317450d348cdf5b512b1b
Aadyaa Sundriyal <you@example.com>
2025-02-25 12:55:37 +0530

    login complete

---
+++ client/src/service/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+const URL='https://localhost:8000';
+export const authenticateSignup=async(data)=>{
+    try{
+        return await axios.post(`$(URL)/signup`,data);
+    } catch(error){
+        console.log('Error',error);
+    }
+}
