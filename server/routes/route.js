6166b2b09e459a5770127e1619026ca86fd6dcd0
Aadyaa Sundriyal <you@example.com>
2025-03-23 13:57:43 +0530

    My message

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,14 +1,10 @@
 import express from 'express';
 
 import { userSignUp ,userLogin} from '../controller/user-controller.js';
-import { getProducts } from '../controller/product-controller.js';
 
 const router = express.Router();
 
 router.post('/signup', userSignUp);
 router.post('/login',userLogin);
-
-router.get('/products', getProducts);
-// router.get('/product/:id', getProductById);
 
 export default router;
