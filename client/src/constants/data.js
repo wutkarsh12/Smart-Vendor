a694faa1896281e1432d73e849ca643aaadc0eed
Aadyaa Sundriyal <sundriyalaadyaa77@gmail.com>
2025-04-14 22:50:36 +0530

    Navbar Changed

--- client/src/constants/data.js
+++ client/src/constants/data.js
@@ -1,15 +1,20 @@
 import saleImage from '../images/sale.jpg';
+import topOffers from '../images/offers.png';
+import grocery from '../images/grocery.png';
+import mobile from '../images/mobile.png';
+import fashion from '../images/fashion.png';
+import electronics from '../images/electronics.png';
+import appliances from '../images/fan.png';
+import beauty from '../images/toys.png';
 
 export const navData = [
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100', text: 'Top Offers' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100', text: 'Grocery' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100', text: 'Mobile' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/82b3ca5fb2301045.png?q=100', text: 'Fashion' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100', text: 'Electronics' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/ee162bad964c46ae.png?q=100', text: 'Home' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100', text: 'Appliances' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100', text: 'Travel' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100', text: 'Beauty, Toys & More' }
+    { url: topOffers, text: 'Top Offers' },
+    { url: grocery, text: 'Grocery' },
+    { url: mobile, text: 'Mobile' },
+    { url: fashion, text: 'Fashion' },
+    { url: electronics, text: 'Electronics' },
+    { url: appliances, text: 'Appliances' },
+    { url: beauty, text: 'Beauty, Toys & More' }
 ];
 
 export const bannerData = [
